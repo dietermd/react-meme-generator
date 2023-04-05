26f4813c7d608e38086d64b5518f3dd2358ed317
Dieter Mutiz <dieter_md@hotmail.com>
2023-04-04 22:50:31 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="images/troll-face.png" alt="" className="header--img" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">A React Project</h4>
+    </header>
+  );
+}
